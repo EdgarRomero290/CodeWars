082e43182132e4dcc389a256890c4ab0962b0773
Edgar Romero <81433282+EdgarRomero290@users.noreply.github.com>
2023-02-01 17:03:08 -0400

    Update Simple Pig Latin.py

--- Simple Pig Latin.py
+++ Simple Pig Latin.py
@@ -16,11 +16,4 @@ def pig_it(text):
             r.append(x[0])   
     return " ".join(r)
 
-######## Mejor Forma #####
 
-#def pig_it(text):
-    #lst = text.split()
-    #return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
-
-#def pig_it(text):
-    #return " ".join(x[1:] + x[0] + "ay" if x.isalnum() else x for x in text.split())
